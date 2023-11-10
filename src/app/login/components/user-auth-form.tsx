847bab752890a3976f25a67f8b45a0d375d2e4db
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-11-10 15:15:44 -0300

    feat: add login page

---
+++ src/app/login/components/user-auth-form.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { LuLogIn } from "react-icons/lu";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="Digite seu email..."
+              type="email"
+              autoCapitalize="none"
+              autoComplete="off"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="senha">
+              Senha
+            </Label>
+            <Input
+              id="email"
+              placeholder="Digite sua senha..."
+              type="password"
+              autoCapitalize="none"
+              autoComplete="off"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            <span className="flex items-center gap-2">
+              Login <LuLogIn size={20} />
+            </span>
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,46 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+import { UserAuthForm } from "./components/user-auth-form";
+
+export const metadata: Metadata = {
+  title: "O Pedal Café - Login",
+  description: "Página de Login do Pedal Café",
+};
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className="container relative h-screen flex flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="bg-login absolute inset-0 bg-cover bg-center" />
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 max-w-md">
+            <div className="flex flex-col space-y-2 text-center">
+              <Image
+                src="/logo.png"
+                alt="Logo O Pedal Café"
+                sizes="100vw"
+                height={0}
+                width={0}
+                className="mx-auto h-auto w-72 object-cover"
+              />
+            </div>
+            <UserAuthForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              Ainda não possui cadastro? {" "}
+              <Link
+                href="/register"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Crie sua conta
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
